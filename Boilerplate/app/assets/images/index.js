57c2c63402068d1639f8b39ab9ee837394868fd2
Praveen Deshmukh <praveen.deshmukh88@gmail.com>
2019-10-20 00:38:07 +0530

    Add assets

--- Boilerplate/app/assets/images/index.js
+++ Boilerplate/app/assets/images/index.js
@@ -1,5 +1,15 @@
 const contest = require('./contest.png');
+const splash = require('./Splash.png');
+const slideOne = require('./page_one.png');
+const slideTwo = require('./page_two.png');
+const slideThree = require('./page_three.png');
+const LogoWithText = require('./Logo_with_text.png');
 
 export const images = {
   contest,
+  splash,
+  slideOne,
+  slideTwo,
+  slideThree,
+  LogoWithText,
 };
