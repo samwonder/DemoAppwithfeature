31eac4cdacc25c47243db05ae030e01f45423524
Praveen Deshmukh <praveen.deshmukh88@gmail.com>
2019-10-23 01:05:48 +0530

    Implement contact list UI

---
+++ Boilerplate/app/screens/ContactList/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { TouchableOpacity, View, StyleSheet,Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  
+});
+
+const HeaderView = (props) => {
+  return (
+    <View style={{flex: 1, }}>
+      <View style={{flex: 1, flexDirection: 'row'}}>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} >
+      <Text style={{fontSize: 26, color: 'gray',}}>Invite</Text>
+      </View>
+      <View style={{flex: 1, right: 20, justifyContent: 'center', alignItems: 'center', position: 'absolute'}}>
+      <TouchableOpacity
+        style={{padding: 10}}
+        onPress={() => alert('WIP')}
+        // onPress={() => props.showPassowrdText()}
+      >
+        <Text style={{fontSize: 20, color: 'gray',}}>Skip</Text>
+      </TouchableOpacity>
+      </View>
+      </View>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{fontSize: 20, color: 'gray',}}>Co-worker and Friends</Text>
+      </View>
+    </View>
+  );
+};
+
+export default HeaderView;
---
+++ Boilerplate/app/screens/ContactList/index.js
@@ -0,0 +1,162 @@
+import React, { Component } from "react";
+import {
+  PermissionsAndroid,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from "react-native";
+import Contacts from "react-native-contacts";
+
+import ListItem from "./components/ListItem";
+import Avatar from "./components/Avatar";
+import SearchBar from "./components/SearchBar";
+import HeaderView from './components/Header';
+
+class ContactList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.search = this.search.bind(this);
+
+    this.state = {
+      contacts: [],
+      searchPlaceholder: "Search"
+    };
+  }
+
+  async componentdidMount() {
+    if (Platform.OS === "android") {
+      PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS, {
+        title: "Contacts",
+        message: "This app would like to view your contacts."
+      }).then(() => {
+        this.loadContacts();
+      });
+    } else {
+      this.loadContacts();
+    }
+  }
+
+  loadContacts() {
+    Contacts.getAll((err, contacts) => {
+      if (err === "denied") {
+        console.warn("Permission to access contacts was denied");
+      } else {
+        this.setState({ contacts });
+      }
+    });
+
+    Contacts.getCount(count => {
+      this.setState({ searchPlaceholder: `Search ${count} contacts` });
+    });
+  }
+
+  search(text) {
+    const phoneNumberRegex = /\b[\+]?[(]?[0-9]{2,6}[)]?[-\s\.]?[-\s\/\.0-9]{3,15}\b/m;
+    const emailAddressRegex = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    if (text === "" || text === null) {
+      this.loadContacts();
+    } else if (phoneNumberRegex.test(text)) {
+      Contacts.getContactsByPhoneNumber(text, (err, contacts) => {
+        this.setState({ contacts });
+      });
+    } else if (emailAddressRegex.test(text)) {
+      Contacts.getContactsByEmailAddress(text, (err, contacts) => {
+        this.setState({ contacts });
+      });
+    } else {
+      Contacts.getContactsMatchingString(text, (err, contacts) => {
+        this.setState({ contacts });
+      });
+    }
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={{height: 90, width: '100%'}} >
+          <HeaderView />
+        </View>
+        <View
+          style={{
+            paddingLeft: 100,
+            paddingRight: 100,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: 'red',
+          }}
+        >
+          {/* <Image
+            source={require("./logo.png")}
+            style={{
+              aspectRatio: 6,
+              resizeMode: "contain"
+            }}
+          /> */}
+        </View>
+        <SearchBar
+          searchPlaceholder={this.state.searchPlaceholder}
+          onChangeText={this.search}
+        />
+        <ScrollView style={{ flex: 1 }}>
+          {this.state.contacts.map(contact => {
+            return (
+              <ListItem
+                leftElement={
+                  <Avatar
+                    img={
+                      contact.hasThumbnail
+                        ? { uri: contact.thumbnailPath }
+                        : undefined
+                    }
+                    placeholder={getAvatarInitials(
+                      `${contact.givenName} ${contact.familyName}`
+                    )}
+                    width={40}
+                    height={40}
+                  />
+                }
+                key={contact.recordID}
+                title={`${contact.givenName} ${contact.familyName}`}
+                description={`${contact.company}`}
+                onPress={() => Contacts.openExistingContact(contact, () => {})}
+                onDelete={() =>
+                  Contacts.deleteContact(contact, () => {
+                    this.loadContacts();
+                  })
+                }
+              />
+            );
+          })}
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+const getAvatarInitials = textString => {
+  if (!textString) return "";
+
+  const text = textString.trim();
+
+  const textSplit = text.split(" ");
+
+  if (textSplit.length <= 1) return text.charAt(0);
+
+  const initials =
+    textSplit[0].charAt(0) + textSplit[textSplit.length - 1].charAt(0);
+
+  return initials;
+};
+
+export default ContactList;
