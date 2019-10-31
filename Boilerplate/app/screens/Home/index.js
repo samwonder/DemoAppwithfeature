593c16a9fc5762836b1fc9ff0c131197f7dac0fc
Praveen Deshmukh <praveen.deshmukh88@gmail.com>
2019-10-31 11:04:12 +0530

    Implement tabbar

--- Boilerplate/app/screens/Home/index.js
+++ Boilerplate/app/screens/Home/index.js
@@ -9,8 +9,56 @@ import PropTypes from 'prop-types';
 import {
   View,
   Dimensions,
+  Image,
+  StyleSheet,
+  Text,
 } from 'react-native';
+import TabNavigator from 'react-native-tab-navigator';
+import { images } from '../../assets/images';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+
+class Home extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Home
+        </Text>
+      </View>
+    )
+  }
+}
+
+class Profile extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Profile
+        </Text>
+      </View>
+    )
+  }
+}
 class Login extends Component {
   constructor(props) {
     super(props);
@@ -19,13 +67,60 @@ class Login extends Component {
   }
 
   componentDidMount() {
-    Navigation.sharedInstance().resetRouteName('AuthenticationScreen');
+    // Navigation.sharedInstance().resetRouteName('AuthenticationScreen');
   }
 
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: 'blue' }}>
-      </View>
+      // <View style={{ flex: 1, backgroundColor: 'blue' }}>
+        <TabNavigator>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'home'}
+            title="Home"
+            // renderIcon={() => <Image source={images.splash} />}
+            // renderSelectedIcon={() => <Image source={images.splash} />}
+            // badgeText="1"
+            onPress={() => this.setState({ selectedTab: 'home' })}>
+            <Home />
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'profile'}
+            title="Profile"
+            // renderIcon={() => <Image source={images.slideOne} />}
+            // renderSelectedIcon={() => <Image source={images.slideOne} />}
+            // renderBadge={() => <CustomBadgeView />}
+            onPress={() => this.setState({ selectedTab: 'profile' })}>
+            <Profile />
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'profile'}
+            title="Profile"
+            // renderIcon={() => <Image source={images.slideOne} />}
+            // renderSelectedIcon={() => <Image source={images.slideOne} />}
+            // renderBadge={() => <CustomBadgeView />}
+            onPress={() => this.setState({ selectedTab: 'profile' })}>
+            <Profile />
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'profile'}
+            title="Profile"
+            // renderIcon={() => <Image source={images.slideOne} />}
+            // renderSelectedIcon={() => <Image source={images.slideOne} />}
+            // renderBadge={() => <CustomBadgeView />}
+            onPress={() => this.setState({ selectedTab: 'profile' })}>
+            <Profile />
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'profile'}
+            title="Profile"
+            // renderIcon={() => <Image source={images.slideOne} />}
+            // renderSelectedIcon={() => <Image source={images.slideOne} />}
+            // renderBadge={() => <CustomBadgeView />}
+            onPress={() => this.setState({ selectedTab: 'profile' })}>
+            <Profile />
+          </TabNavigator.Item>
+        </TabNavigator>
+      // </View>
     );
   }
 }
